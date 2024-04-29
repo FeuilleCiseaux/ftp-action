43b3c4e9c06d0797ddd2365fb5062aae18346667
Pierre <103735886+FeuilleCiseaux@users.noreply.github.com>
2024-04-29 10:29:37 +0200

    Update entrypoint.sh
    Test pour voir si la connexion seule passe

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-lftp $INPUT_HOST -p 22 -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror $INPUT_OPTIONS --continue --dereference $INPUT_REMOTEDIR $INPUT_LOCALDIR; quit"
+lftp $INPUT_HOST -p 22 -u $INPUT_USER,$INPUT_PASSWORD
