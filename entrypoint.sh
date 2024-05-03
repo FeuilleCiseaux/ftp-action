7b859555385fb5d327fe2c86d913d1f5890ac573
Pierre <103735886+FeuilleCiseaux@users.noreply.github.com>
2024-05-03 17:18:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
-ls
+lftp <<EOF 
+set ssl:verify-certificate no  
+set sftp:auto-confirm yes
+open $INPUT_HOST -p 22 -u $INPUT_USER,$INPUT_PASSWORD
+mirror $INPUT_REMOTEDIR $INPUT_LOCALDIR
+EOF
